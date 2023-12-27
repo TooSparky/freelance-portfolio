dac224d587d1f08f41be1e93a7d1c1d73957720d
Zach Barnes <16zacharybarnes@gmail.com>
2023-12-27 16:03:26 -0600

    added footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,7 +1,25 @@
+import './styles.css'
+
 export default function Footer() {
     return(
-        <footer>
-
+        <footer id='footer'>
+            <div className='footer-top-row'>
+                <div className='footer-column-1'>
+                    <p>example text 1</p>
+                </div>
+                <div className='footer-column-2'>
+                    <p>example text 2</p>
+                </div>
+                <div className='footer-column-3'>
+                    <p>example text 3</p>
+                </div>
+                <div className='footer-column-4'>
+                    <p>example text 4</p>
+                </div>
+            </div>
+            <div className='footer-bottom-row'>
+                <h3><i className="fa-regular fa-copyright"></i> 2023 QuantumSites. All rights reserved.</h3>
+            </div>
         </footer>
     );
 }
