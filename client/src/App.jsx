be6b46bc4ed3884783d6d665e451a1beb269cb7e
Zach Barnes <16zacharybarnes@gmail.com>
2023-12-27 13:57:43 -0600

    added responsive background

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,7 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+import net from 'vanta/src/vanta.net'
 
 import Navbar from './components/navbar/Navbar'
 import Footer from './components/footer/Footer'
@@ -11,6 +13,24 @@ const client = new ApolloClient({
 });
 
 function App() {
+  useEffect(() => {
+    net({
+      el: '#root',
+      speed: 1.5,
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      color: 0x1300d7,
+      backgroundColor: 0x3e,
+      points: 8.00,
+      maxDistance: 18.00,
+      spacing: 12.00
+    })
+  }, [])
   return (
     <ApolloProvider client={client}>
       <Navbar />
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,13 +2,13 @@ import './styles.css';
 
 export default function Navbar() {
     return(
-        <div className='nav-container'>
+        <div className='nav-container sticky'>
             <nav id="nav">
                 <ul className='links'>
-                    <li><a href="#">About Us</a></li>
-                    <li><a href="#">Services</a></li>
+                    <li><a href="#team-section">About Us</a></li>
+                    <li><a href="#services-section">Services</a></li>
                 </ul>
-                <div className='logo'><a href='#'>QuantumSites</a></div>
+                <div className='logo'><a href='#header-section'>QuantumSites</a></div>
 
                 <a href='#' className='button'>Get Started</a>
             </nav>
